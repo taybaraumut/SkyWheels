c430cd63abf6324ece7d8a2288a355635f2e3034
Umut <umut.taybara.info@gmail.com>
2025-01-02 21:27:34 +0300

    Add MSSQL database connection

---
+++ src/Services/ContactService/SkyWheels.Contact.API/Context/ContactContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace SkyWheels.Contact.API.Context
+{
+    public class ContactContext
+    {
+        private readonly IConfiguration configuration;
+        private readonly string _connectionString;
+
+        public ContactContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            _connectionString = configuration.GetConnectionString("connection")!;
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
