a05da6c908f351b3d756c7f10f672849ec7a7233
Umut <umut.taybara.info@gmail.com>
2025-01-02 21:40:53 +0300

    Made first code edits

---
+++ src/Services/ContactService/SkyWheels.Contact.API/Extensions/MiddlewareExtensions/MiddlewareExtension.cs
@@ -0,0 +1,15 @@
+﻿using SkyWheels.Contact.API.Middlewares;
+
+namespace SkyWheels.Contact.API.Extensions.MiddlewareExtensions
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ContactValidationMiddleware>();
+            app.UseMiddleware<GlobalErrorHandlingMiddleware>();
+
+            return app;
+        }
+    }
+}
---
+++ src/Services/ContactService/SkyWheels.Contact.API/Extensions/ServiceExtensions/ServiceExtension.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using SkyWheels.Contact.API.Context;
+using SkyWheels.Contact.API.Dtos;
+using SkyWheels.Contact.API.Middlewares;
+using SkyWheels.Contact.API.Services;
+using SkyWheels.Contact.API.ValidationRules;
+
+namespace SkyWheels.Contact.API.Extensions.ServiceExtensions
+{
+    public static class ServiceExtension
+    {
+        public static WebApplicationBuilder AddCustomServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IValidator<CreateContactDto>, CreateContactValidator>();
+            builder.Services.AddScoped<IValidator<UpdateContactDto>, UpdateContactValidator>();
+            builder.Services.AddScoped<IContactService,ContactService>();
+            builder.Services.AddSingleton<ContactContext>();
+            builder.Services.AddTransient<GlobalErrorHandlingMiddleware>();
+            builder.Services.AddTransient<ContactValidationMiddleware>();
+            //builder.Services.AddScoped<DataProtector>();
+            builder.Services.AddDataProtection();
+            return builder;
+        }
+    }
+}
