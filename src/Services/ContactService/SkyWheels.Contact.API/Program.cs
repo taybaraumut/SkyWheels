449fe17b1324f8a5443336866e9f553e974da2a0
Umut <umut.taybara.info@gmail.com>
2025-01-02 21:23:01 +0300

    Start Project

---
+++ src/Services/ContactService/SkyWheels.Contact.API/Program.cs
@@ -0,0 +1,29 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ServiceExtensions
+// builder.AddCustomServices();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Middleware Extensions
+//app.UseMiddleware();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Services/ReservationService/Tests/UnitTests/SkyWheels.Reservation.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace SkyWheels.Reservation.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
